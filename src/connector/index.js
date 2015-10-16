4ca9146ce8e428805665990c388fdab5924dd77c
Tiago Alves <letiagoalves@gmail.com>
2015-10-16 22:23:55 +0100

    connector getters returning copies + documentation

--- src/connector/index.js
+++ src/connector/index.js
@@ -1,30 +1,57 @@
 'use strict';
 
-// TODO: return copies and only trough getters
 var validator = require('./../validator');
 var schema = require('./schema');
+var clone = require('mout/lang/clone');
 
+/**
+ * @constructor Connector
+ * @description Creates a connector instance that can be attached to block instances
+ * @param {String} id           the connector identifier
+ * @param {String} type         the connector type (blacklist or whitelist)
+ * @param {Array} blockIds      the allowed/denied set of block identifiers
+ * @param {Array} blockClasses  the allowed/denied set of block classes
+ */
 function Connector(id, type, blockIds, blockClasses) {
     id = validator.assert(id, schema.id, 'Connector id');
     type = validator.assert(type, schema.type, 'Connector type');
     blockIds = validator.assert(blockIds, schema.blockIds, 'Connector blocks IDs');
     blockClasses = validator.assert(blockClasses, schema.blockClasses, 'Connector blocks classes');
 
-    // TODO: documentation
+    /**
+     * @function getId
+     * @description Returns the connector identifier
+     * @return {String}
+     */
     function getId() {
         return id;
     }
 
+    /**
+     * @function getType
+     * @description Returns the connector type (blacklist or whitelist)
+     * @return {String}
+     */
     function getType() {
         return type;
     }
 
+    /**
+     * @function getBlockIds
+     * @description Returns the allowed/denied set of block identifiers
+     * @return {Array}
+     */
     function getBlockIds() {
-        return blockIds;
+        return clone(blockIds);
     }
 
+    /**
+     * @function getBlockClasses
+     * @description Returns the allowed/denied set of block classes
+     * @return {Array}
+     */
     function getBlockClasses() {
-        return blockClasses;
+        return clone(blockClasses);
     }
 
     // public
